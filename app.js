62097d0f36bc1208ed3f19590d0a6d05e38c2200
Evangeline Drink <evangelinedrink@gmail.com>
2021-05-11 22:44:05 -0700

    Simple Node Module

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //JavaScript Program that was tested with Node.
-const rect= { //rect stands for rectangle
+/*const rect= { //rect stands for rectangle
     perimeter: (x,y) => 2* (x+y),
     area: (x,y) => x*y
-};
+}; */
+
+const rect= require("./rectangle") //Will look at the same folder (by using ./) and look for the file named rectangle.
+//The perimeter and area from rectangle.js was imported here by using the require() function.  The line above is equivalent to the object in lines 2-5.
 
 function solveRect(l,w) { //l stands for length and w stands for width
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`); //Using Template Literals (because of backticks `)
---
+++ rectangle.js
@@ -0,0 +1,2 @@
+exports.perimeter=(x,y)=> 2*(x+y); //Using exports keyword from Node. We could also write modules.exports instead of just exports.
+exports.area= (x,y) => x*y;
