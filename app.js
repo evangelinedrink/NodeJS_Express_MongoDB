36d498ea59b939ca9420543cfe5292b589e72823
Evangeline Drink <evangelinedrink@gmail.com>
2021-05-12 18:49:18 -0700

    Node Callbacks and Error Handling

--- app.js
+++ app.js
@@ -10,12 +10,17 @@ const rect= require("./rectangle") //Will look at the same folder (by using ./) 
 function solveRect(l,w) { //l stands for length and w stands for width
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`); //Using Template Literals (because of backticks `)
 
-    if (l<=0 || w <=0) {
-        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
-    } else {
-        console.log(`Area of rectangle: ${rect.area(l,w)}`);
-        console.log(`Perimeter of rectangle: ${rect.perimeter(l,w)}`);
-    }
+    //Importing the function from rectangle.js.  We are calling this function here. In rectangle.js, the function accepts two positive values (in the file it is x and y) and the callback funtion. x and y will be l and w in this file.
+    //The callback function is the third parameter for the function from rectangle.js. In the line below we are defining this function. 
+    rect(l,w, (err, rectangle) => { //If l and w (x and y) are positive function, then the callback function (err,rectangle) will run.
+        if (err) {
+            console.log("Error:", err.message);
+        } else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`); //Empty parameter lists (nothing in the parenthesis) because the values are already in the rectangle.js file.
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log("This statement is logged after the call to rect()");
 }
 
 //Call the function to test it
